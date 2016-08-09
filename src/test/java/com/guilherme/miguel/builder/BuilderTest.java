b51178504213fcc5e5adc18ca17d40708aa727f9
Miguel Guilherme <mguilherme82@gmail.com>
2016-08-09 21:21:26 +0100

    Add more tests

--- src/test/java/com/guilherme/miguel/builder/BuilderTest.java
+++ src/test/java/com/guilherme/miguel/builder/BuilderTest.java
@@ -4,13 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
-import static org.hamcrest.core.IsSame.sameInstance;
 
 /**
  * @author Miguel Guilherme
@@ -49,13 +46,44 @@ public class BuilderTest {
     }
 
     @Test
+    public void vegBurgerShouldCost25() throws Exception {
+        assertThat((new VegBurger()).price(), is(equalTo(25.0f)));
+    }
+
+    @Test
+    public void nonVegBurgerShouldCost50() throws Exception {
+        assertThat((new ChickenBurger()).price(), is(equalTo(50.5f)));
+    }
+
+    @Test
+    public void cokeShouldCost30() throws Exception {
+        assertThat((new Coke()).price(), is(equalTo(30.0f)));
+    }
+
+    @Test
+    public void pepsiShouldCost35() throws Exception {
+        assertThat((new Pepsi()).price(), is(equalTo(35.0f)));
+    }
+
+    @Test
     public void vegMealShouldCost55() throws Exception {
         assertThat(vegMeal.getCost(), is(equalTo(55.0f)));
     }
 
     @Test
-    public void nonVegMealShouldCost855() throws Exception {
+    public void nonVegMealShouldCost85And5() throws Exception {
         assertThat(nonVegMeal.getCost(), is(equalTo(85.5f)));
     }
 
+    @Test
+    public void packageShouldReturnWrapper() throws Exception {
+        assertThat((new VegBurger()).packing().pack(), is(equalTo("Wrapper")));
+        assertThat((new ChickenBurger()).packing().pack(), is(equalTo("Wrapper")));
+    }
+
+    @Test
+    public void packageShouldReturnBottle() throws Exception {
+        assertThat((new Coke()).packing().pack(), is(equalTo("Bottle")));
+        assertThat((new Pepsi()).packing().pack(), is(equalTo("Bottle")));
+    }
 }
